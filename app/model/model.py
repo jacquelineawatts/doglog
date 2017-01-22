7eea04b13cfd64df708eb1426f486d32fc1b77e5
Jacqueline Watts <jacquelineawatts@gmail.com>
2017-01-21 19:16:33 -0800

    Added local db connection back to model.py

--- app/model/model.py
+++ app/model/model.py
@@ -12,19 +12,19 @@ url = urlparse.urlparse(os.environ['DATABASE_URL'])
 def connect_to_db(app, db_uri="postgresql:///doglog"):
     """Connect application to the db."""
 
-    # ----- CONNECTING TO LOCAL DB: ----------
-
-    # app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    # app.config['SQLALCHEMY_ECHO'] = True
-    # db.app = app
-    # db.init_app(app)
-
     conn = psycopg2.connect(database=url.path[1:],
                             user=url.username,
                             password=url.password,
                             host=url.hostname,
                             port=url.port
                             )
+
+    # ----- CONNECTING TO LOCAL DB: ----------
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.app = app
+    db.init_app(app)
 
 
 if __name__ == '__main__':
